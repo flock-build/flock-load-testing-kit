577d8aca0f16af1bd88e062d0e94e081ee1f27eb
Ramakrishna Natarajan <nkrama99@gmail.com>
2024-05-28 14:04:38 -0400

    correct model list

--- const/model_mapping.py
+++ const/model_mapping.py
@@ -1,28 +1,13 @@
-# MODEL_MAPPING = {
-#     "1": "microsoft/WizardLM-2-7B",
-#     "2": "meta-llama/Meta-Llama-3-8B-Instruct",
-#     "3": "mistralai/Mixtral-8x22B-Instruct-v0.1",
-#     "4": "bigcode/starcoder2-15b",
-#     "5": "openchat/openchat_3.5",
-#     "6": "Gryphe/MythoMax-L2-13b",
-#     "7": "Phind/Phind-CodeLlama-34B-v2",
-#     "8": "llava-hf/llava-1.5-7b-hf",
-#     "9": "google/gemma-1.1-7b-it",
-#     "10": "microsoft/WizardLM-2-8x22B",
-#     "11": "lizpreciatior/lzlv_70b_fp16_hf"
-# }
-
 MODEL_MAPPING = {
     "1": "microsoft/WizardLM-2-7B",
     "2": "meta-llama/Meta-Llama-3-8B-Instruct",
-    "3": "meta-llama/Meta-Llama-3-70B-Instruct",
-    "4": "mistralai/Mistral-7B-Instruct-v0.2",
-    "5": "google/gemma-1.1-7b-it",
+    "3": "mistralai/Mixtral-8x22B-Instruct-v0.1",
+    "4": "lizpreciatior/lzlv_70b_fp16_hf",
+    "5": "llava-hf/llava-1.5-7b-hf",
     "6": "Gryphe/MythoMax-L2-13b",
-    "7": "Phind/Phind-CodeLlama-34B-v2",
-    "8": "llava-hf/llava-1.5-7b-hf",
-    "9": "google/gemma-1.1-7b-it",
-    "10": "microsoft/WizardLM-2-8x22B",
-    "11": "lizpreciatior/lzlv_70b_fp16_hf",
-    "12": "mistralai/Mixtral-8x22B-Instruct-v0.1",
+    "7": "openchat/openchat_3.5",
+    "8": "openai/whisper-tiny",
+    "9": "microsoft/WizardLM-2-8x22B",
+    "10": "google/gemma-1.1-7b-it",
+    "11": "Phind/Phind-CodeLlama-34B-v2",
 }
